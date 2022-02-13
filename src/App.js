18e4ddc8a6ed0671398d5c8654f7a9981952253d
Gabriel Rizzo <daudtrizzo@gmail.com>
2022-02-13 00:14:23 -0300

    revert teste

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
